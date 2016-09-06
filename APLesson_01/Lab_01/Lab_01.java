99b622aec260733268a87cf703384107aca6cdf3
dorlokk <robodudeag@gmail.com>
2016-09-06 07:51:41 -0700

    added files

---
+++ APLesson_01/Lab_01/Lab_01.java
@@ -0,0 +1,10 @@
+public class Lab_01
+{
+	public static void main(String[]args)
+	{
+		System.out.println("   @..@");
+		System.out.println("    (----)");
+		System.out.println("  ( >__< )");
+		System.out.println("^^ ~~ ^^");
+	}
+}
